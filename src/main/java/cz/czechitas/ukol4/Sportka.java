3a829eb29958e3fff9316569abba9015bee65482
luca-trnka <luie-trnkova76@seznam.cz>
2022-05-15 15:55:46 +0200

    hotový úkol

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -11,7 +11,9 @@ public class Sportka {
   private final List<Integer> osudi = new ArrayList<>();
 
   public Sportka() {
-    //TODO naplnit osudí čísly 1 až 49.
+    for (int i = 1; i < 50; i++) {
+      osudi.add(i);
+    }
   }
 
   /**
@@ -20,7 +22,7 @@ public class Sportka {
    * @see Collections#shuffle(List)
    */
   public void zamichat() {
-    //TODO zamíchat osudí
+    Collections.shuffle(osudi);
   }
 
   /**
@@ -31,8 +33,7 @@ public class Sportka {
    * @see List#subList(int, int)
    */
   public List<Integer> vylosovanaCisla() {
-    //TODO Vrátit seznam prvních 6 čísel z osudí.
-    return null;
+    return osudi.subList(0, 6);
   }
 
   /**
@@ -41,7 +42,7 @@ public class Sportka {
    * @return Dodatkové číslo.
    */
   public Integer dodatkoveCislo() {
-    //TODO Vrátit sedmé číslo z osudí.
-    return null;
+    return osudi.get(7);
   }
+
 }
